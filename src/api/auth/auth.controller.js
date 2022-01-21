6ce33e6220fb7b7b4d0091a4c1312ec4e151f2a8
Nguyễn Minh Trí <21522706@gm.uit.edu.vn>
2022-01-21 21:12:20 +0700

    Login

--- src/api/auth/auth.controller.js
+++ src/api/auth/auth.controller.js
@@ -1,8 +1,20 @@
+const authService = require("./auth.service");
+
 module.exports = {
+  
   login: async (req, res) => {
+    let DTO = await authService.login(req.body);
+    
+    
+    if(DTO.error){
+        res.status(500).json(DTO.msg);
+        return;
+    }
+    
+    
     res.status(200).json({
-      message: 'Login successful',
-      token: 'This is jwt token',
+      message: DTO.msg,
+      token: DTO.token
     });
   },
   signup: async (req, res) => {
