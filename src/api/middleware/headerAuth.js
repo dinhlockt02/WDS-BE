4017181f994945cf77ca85f9b4b45074bff9cd09
kietngo255 <91515708+kietngo255@users.noreply.github.com>
2022-01-24 02:09:41 +0700

    .

--- src/api/middleware/headerAuth.js
+++ src/api/middleware/headerAuth.js
@@ -3,8 +3,8 @@ var headerAuth = req.headers.authorization;
 
 if (headerAuth) {
  var token = req.headers.authorization.split(' ')[1]; 
-    jwt.verify(token, 'secret', function(error, decoded) {
-      if (error) {
+    jwt.verify(token, 'secret', function(err, decoded) {
+      if (err) {
         res.status(401).json('Unauthorized');
       } else {
         req.user = decoded.username;
